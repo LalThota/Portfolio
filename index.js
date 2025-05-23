6b029e3024a02d61e6c2213f95fc2ef83e035047
LalThota <lal98thota@gmail.com>
2025-05-23 20:44:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
 
-
-function type() {
-  if (charIndex < text[index].length) {
-    span.textContent += text[index].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, 100);
-  } else {
-    setTimeout(erase, 1500);
-  }
-}
-
-function erase() {
-  if (charIndex > 0) {
-    span.textContent = text[index].substring(0, charIndex - 1);
-    charIndex--;
-    setTimeout(erase, 50);
-  } else {
-    index = (index + 1) % text.length;
-    setTimeout(type, 200);
-  }
-}
-
 document.addEventListener("DOMContentLoaded", type);
-
-// Menu toggle
 document.getElementById("menu-toggle").addEventListener("click", () => {
   document.getElementById("nav-list").classList.toggle("show");
 });
